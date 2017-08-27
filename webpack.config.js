6a383659e00c7ff4acc6653e25d5e12f08a31daa
Alvarez, Jorge <Jorge_Alvarez@intuit.com>
2017-08-27 15:46:03 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const path = require("path");
+
+const config = {
+  devtool: "source-map",
+  entry: {
+    "app": [
+      "react-hot-loader/patch",
+      "./src/index.jsx"
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+
+      {
+        test: /\.svg$/,
+        loader: "svg-url-loader"
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          },
+        ]
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              data: '@import "./sass-config.scss";',
+              includePaths: [
+                path.join(__dirname, '/')
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+};
+
+module.exports = config;
